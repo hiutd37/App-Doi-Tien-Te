2c307300beb34674f8e4743474c14f0817f33a40
Hieu Ninh <ninhngochieu@gmail.com>
2020-06-09 21:43:28 +0700

    Revert "Merge branch 'fix-GUI'"
    This reverts commit ca1d874dd9a4ce6030139f3d504f9c9d579b4d79, reversing
    changes made to b547b656fa1698e2be0e07a1c8e3cbf917259ede.

--- app/src/main/java/com/example/appdoitiente/MainActivity.java
+++ app/src/main/java/com/example/appdoitiente/MainActivity.java
@@ -3,67 +3,25 @@ package com.example.appdoitiente;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    Spinner spinner1;
-    Spinner spinner2;
-    ArrayAdapter<CharSequence> adapterSpinner;
-    ArrayAdapter adapterList;
-    ListView listView;
-    Button buttonConvert;
-    EditText editTextCurrency;
-    ArrayList list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addControls();
-        addEvents();
+        Spinner spinner1 = (Spinner) findViewById(R.id.simpleSpinner1);
+        Spinner spinner2 = (Spinner) findViewById(R.id.simpleSpinner2);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner1.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
     }
 
-    private void addEvents() {
-        list= new ArrayList();
-        list.add("VND");
-        list.add("US Dollar");
-        list.add("YEN");
-        list.add("WON");
-
-        adapterSpinner = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,list);
-        adapterList= new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
-        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        listView.setAdapter(adapterList);
-        spinner1.setAdapter(adapterSpinner);
-        spinner2.setAdapter(adapterSpinner);
-
-        buttonConvert.setOnClickListener(this);
-
-    }
-
-    private void addControls() {
-        spinner1 = (Spinner) findViewById(R.id.simpleSpinner1);
-        spinner2 = (Spinner) findViewById(R.id.simpleSpinner2);
-        listView=findViewById(R.id.listView);
-        buttonConvert=findViewById(R.id.buttonConvert);
-        editTextCurrency= findViewById(R.id.editTextCurrency);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.buttonConvert:
-                Toast.makeText(this,"ádadasda",Toast.LENGTH_LONG).show();
-        }
-    }
 }
